e062a304487967e741aae5bf30522e73d19d7340
Monir Imamverdi <monir.imamverdi@gmail.com>
2015-06-20 22:32:54 -0600

    Beautiful Soup features started

--- carrierAnalyzer.py
+++ carrierAnalyzer.py
@@ -1,18 +1,45 @@
 """
-This module is used to analyze carrier information for Canadian phone numbers.
+This module is used to analyze carrier information given a Canadian phone numbers.
 
 This module will be referencing area code/prefix data (aka. NPA/NXX codes)
-provided by the Canadian Numbering here: http://www.cnac.ca/co_codes/co_code_status.htm
+provided by the Canadian Numbering Administrator here: http://www.cnac.ca/co_codes/co_code_status.htm
+where we load the page based on a given area code, then search the page for corresponding CO Code.
+
+Example:
+Number: (780) 200 1010
+Then, area code = 780 and CO code = 200.
+So what we have is 780 corresponding to Edmonton, AB and 200 correponding to WIND Mobile Corp.
+
+The website is scraped using the Beautiful Soup library.
 
 Note on phone number portability:
 In Canada, phone numbers are portable, meaning that owners can change carriers while
 keeping their existing phone numbers. This carrier lookup method only returns the
 ORIGINAL carrier for a given number.
+
+Attributes:
+    baseURL (str): Contains base URL for Canadian Numbering Administrator site containing area code carrier information
+    carrierName (str): Carrier name will be stored in this once resolved 
 """
 
 import phonenumbers
+import urllib.request
+from bs4 import BeautifulSoup
 
 
 class CarrierAnalyzer():
     def __init__(self):
-        self.carrier = ""
+        """Return base URL used for scraping carrier information and carrier name"""
+        self.baseURL = "http://www.cnac.ca/data/COCodeStatus_NPA" # followed by "[areaCode].htm"
+        self.carrierName = None # carrier initially null
+
+    def analyzeNumber(self, number):
+        """Recieve Canadian phone number and determine carrier name"""
+        areaCode = number[0,2] # first 3 digits of a national number represent the area code
+        COCode = number[3,5] # second 3 digits of a national number represent the CO Code (carrier indicator)
+
+        soup = BeatifulSoup(urllib.request.urlopen(baseURL+areaCode+".htm").read())
+
+
+
+
--- message.py
+++ message.py
@@ -7,6 +7,7 @@ phone number from the user to send.
 
 Attributes:
     phoneNo (PhoneNumber): PhoneNumber object containing parsed recipient phone number
+    nationalNo (str): Phone number excluding country code
     carrier (str) : String containing name of carrier
     message (str): Message text content to be sent
 
@@ -15,17 +16,28 @@ Attributes:
 'hi'
 >>> str(x.get_recipient())
 'Country Code: 1 National Number: 7802001714'
+>>> x.nationalNo
+'7802001714'
+
+>>> x = Message("7802001714", "hi")
+>>> x.message
+'hi'
+>>> str(x.get_recipient())
+'Country Code: 1 National Number: 7802001714'
+>>> x.nationalNo
+'7802001714'
 """
 
 import phonenumbers
 
 
 class Message():
-    def __init__(self, recipient=0, message_text=""):
+    def __init__(self, recipient="", message_text=""):
         """We cast phone the received number input as string. Both attributes are given default values"""
-        self.phoneNo = phonenumbers.parse(str(recipient), "CA")
-        self.message = message_text
         self.recipient = ""
+        self.phoneNo = phonenumbers.parse(str(recipient), "CA")
+        self.nationalNo = phonenumbers.national_significant_number(self.phoneNo)
+        self.message = message_text
 
     def get_recipient(self):
         """Returns recipient's phone number"""
